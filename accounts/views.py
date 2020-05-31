d0dca2f93d88e2d4b7fbe6e6acfad5b446fa9568
Muhammad Bilal <rana123bilal@gmail.com>
2020-05-31 20:17:08 +0500

    Real Estate Website

---
+++ accounts/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from django.contrib import auth
+from django.contrib.auth.models import User
+
+from contacts.models import Contact
+def register(request):
+    if request.method == 'POST':
+        #Get form Values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+    #check if passowrd match
+
+        if password == password2:
+          #check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is already taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is already taken')
+                    return redirect('register')
+                else:
+                    #look good
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request,'You are now registered and can login')
+                    return redirect('login')
+        else:
+            messages.error(request, 'password did not match')
+            return redirect('register')
+    else:
+            return render(request, 'accounts/register.html')
+
+def login(request):
+        if request.method == 'POST':
+            username=request.POST['username']
+            password=request.POST['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request,'You are now login')
+                return redirect('dashboard')
+            else:
+                messages.error(request,'Invalid Credentials')
+                return redirect('login')
+        else:
+         return render(request, 'accounts/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request,'You are now Logout')
+    return redirect('index')
+
+def dashboard(request):
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id = request.user.id)
+
+    context = {
+        'contacts' : user_contacts
+    }
+    return render(request, 'accounts/dashboard.html' , context)
